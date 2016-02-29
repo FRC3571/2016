9b4fc49a2623fa8ce93b354bb1921002d529d670
TomasR <Rakusan2@gmail.com>
2016-02-28 23:57:08 -0500

    Added buttons for stop, reverse, and boost

--- src/org/usfirst/frc/team3571/robot/Teleop.java
+++ src/org/usfirst/frc/team3571/robot/Teleop.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team3571.robot;
 
 public class Teleop extends OI {
 	
+	private static double driveY=0, driveX=0;
+	
 	/**
 	 * Runs once
 	 */
@@ -12,13 +14,31 @@ public class Teleop extends OI {
 	 * Runs at a maximum of 50Hz during Teleop
 	 */
 	public static void periodic(){
+		driveX = driver.LeftStick.X;
+		driveY = driver.LeftStick.Y;
 		
-		drive.arcadeDrive(driver.LeftStick.Y,driver.LeftStick.X);
+		if(driver.Buttons.A.current)driveY = -driveY;//Useful for driving in reverse with minimal training
+		
+		if(!driver.Buttons.LeftStick.current){
+			//Trades is a little bit of speed for nicer cornering
+			//Pressing the left stick or the button underneath disables this
+			driveX *= 0.8;
+			driveY *= 0.8;
+		}
+		
+		if(driver.Buttons.B.current){//A stop button
+			drive.stopMotor();
+		}
+		else{
+			drive.arcadeDrive(driveY,driveX);
+		}
 		
 		arm.set(driver.RightStick.Y * 0.75);
 		
+		//Sets intake speed
 		ballIntake.set(driver.Triggers.Combined);
 		
+		//Moves the lift mechanism
 		lift.set(operator.LeftStick.Y);
 		
 	}
